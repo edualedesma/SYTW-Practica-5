564d50ebac69954f91fd2f31624a8587afd49f71
Sergio <alu0100696615@ull.edu.es>
2014-11-03 17:24:37 +0000

    Añadiendo tabla visit. Falta definir funciones

--- model.rb
+++ model.rb
@@ -5,4 +5,25 @@ class ShortenedUrl
   property :url, Text
   property :myurl, String
   property :username, Text
+  
+  has n, :visits
+end
+
+
+class Visit
+  include DataMapper::Resource
+
+  property  :id,          Serial
+  property  :created_at,  DateTime
+  property  :ip,          IPAddress
+  property  :country,     String
+  belongs_to  :link
+
+  after :create, :set_country
+
+  def set_country
+
+  end
+
+                                   
 end
