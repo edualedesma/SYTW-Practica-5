b51232f19af6317b62dcac91ef4e6b3dae306c04
Sergio <alu0100696615@ull.edu.es>
2014-11-07 11:53:45 +0000

    Añadido grafica de países y funcion en el model.rb

--- model.rb
+++ model.rb
@@ -18,16 +18,19 @@ class Visit
   property  :id,          Serial
   property  :created_at,  DateTime
   property  :ip,          IPAddress
- # property  :country,     String
+  property  :country,     String
   
   belongs_to  :shortenedurl
 
-  #after :create, :set_country
+
+  before :create, :set_country
 
   def set_country
     xml = RestClient.get "http://ip-api.com/xml/#{self.ip}"  
     pais = XmlSimple.xml_in(xml.to_s, { 'ForceArray' => false })['country'].to_s
-    if (!pais) 
+    p "Este es el pais: "
+    p pais
+    if (pais == "") 
       pais = "No encontrada"
     end
     self.country = pais
@@ -37,5 +40,9 @@ class Visit
   def self.fecha_por_dias(id)
     repository(:default).adapter.select("SELECT date(created_at) AS date, count(*) AS count FROM visits WHERE shortenedurl_id = '#{id}' GROUP BY date(created_at)")
   end
+
+  def self.pais_contador(id)
+    repository(:default).adapter.select("SELECT country, count(*) AS count FROM visits WHERE shortenedurl_id = '#{id}' GROUP BY country")
+  end  
                                    
 end
